0eb9a737c453989656128fd82d8f9a7bac87ecb6
Darren Chang <darren.ch12@gmail.com>
2020-10-24 16:26:54 -0400

    EDA add data and transformation.

--- eda_darren.R
+++ eda_darren.R
@@ -1,22 +1,16 @@
-# library(tidyverse)
-# install.packages("devtools")
-# library(devtools)
-# #library(ellipsis)
-# # install.packages("ellipsis")
-# devtools::install_github(repo = "maksimhorowitz/nflscrapR")
-# detach("package:namespace", unload = T, character.only = T)
-
-# install.packages("processx")
-
-
-# unloadNamespace("namespace")
-
-
 # install.packages("nflfastR")
 library(nflfastR)
 library(tidyverse)
 library(vroom)
-wk1_2019 <- vroom("C:\\Users\\darre\\Documents\\_cornell 20-21\\orie 4741\\data_v2\\weekly\\2019\\week17.csv")
+
+setwd("C:\\Users\\darre\\Documents\\_cornell 20-21\\orie 4741\\data_v2\\weekly\\2019")
+
+temp <- list.files(pattern="*.csv", recursive = T)
+weekly_ff <- lapply(temp, vroom)
+weekly_ff <- weekly_ff %>% 
+            bind_rows(.id = "column_label")
+# wk1_2019 <- vroom("C:\\Users\\darre\\Documents\\_cornell 20-21\\orie 4741\\data_v2\\weekly\\2019\\week1.csv")
+
 games_2019 <- vroom("C:\\Users\\darre\\Documents\\_cornell 20-21\\orie 4741\\nflscrapR-data\\games_data\\regular_season\\reg_games_2019.csv")
 
 games_2019 <- games_2019 %>%
@@ -50,10 +44,81 @@ games_2019_concat <- games_2019 %>%
                             team2_score = team1_score,
                             team1_score = team2_score)))
 
-wk1_2019 <- wk1_2019 %>%
-                mutate(Tm = as.factor(Tm)) %>%
-                rename(team1 = Tm) %>% 
-                left_join((games_2019_concat %>% filter(week == 1)), 
-                    by = c("team1" = "team1"))
+# wk1_2019 <- wk1_2019 %>%
+#                 mutate(Tm = as.factor(Tm)) %>%
+#                 rename(team1 = Tm) %>% 
+#                 left_join((games_2019_concat %>% filter(week == 1)), 
+#                     by = c("team1" = "team1"))
 
-View(wk1_2019)
+weekly_ff_2 <- weekly_ff %>%
+                mutate(Tm = as.factor(Tm),
+                        column_label = as.numeric(column_label)) %>%
+                rename(team1 = Tm,
+                    week = column_label) %>% 
+                left_join(games_2019_concat, 
+                    by = c("team1" = "team1", 
+                    "week" = "week"))
+
+weekly_ff_3 <- weekly_ff_2 %>% 
+    group_by(Player) %>% 
+    mutate(across(.cols = -c(week,
+                    Pos, 
+                    team1, 
+                    PPRFantasyPoints, 
+                    StandardFantasyPoints,
+                    HalfPPRFantasyPoints,
+                    type,
+                    game_id,
+                    team2,
+                    season),
+            cummean,
+            .names = "{col}_cum"),
+        across(.cols = -c(week,
+                    Pos, 
+                    team1, 
+                    PPRFantasyPoints, 
+                    StandardFantasyPoints,
+                    HalfPPRFantasyPoints,
+                    type,
+                    game_id,
+                    team2,
+                    season,
+                    ends_with("_cum")),
+            lag,
+            n = 1L,
+            .names = "{col}_prev"),
+        across(.cols = -c(week,
+                    Pos, 
+                    team1, 
+                    PPRFantasyPoints, 
+                    StandardFantasyPoints,
+                    HalfPPRFantasyPoints,
+                    type,
+                    game_id,
+                    team2,
+                    season),
+            replace_na,
+            0)) 
+
+vroom_write(weekly_ff_3,
+            path = "C:\\Users\\darre\\Documents\\_cornell 20-21\\orie 4741\\dream-team\\weekly_ff_3.csv",
+            delim = ",")
+
+
+            
+    #         %>% 
+    # filter_at(vars(c(week,
+    #                 Pos, 
+    #                 team1, 
+    #                 PPRFantasyPoints, 
+    #                 StandardFantasyPoints,
+    #                 HalfPPRFantasyPoints,
+    #                 type,
+    #                 game_id,
+    #                 team2,
+    #                 season)),
+    #                 any_vars(is.na(.)))
+
+
+# standings <- read_csv("http://www.habitatring.com/standings.csv")
+# games <- read_csv("http://www.habitatring.com/games.csv")
