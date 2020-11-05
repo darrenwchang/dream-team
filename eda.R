f12df69deb01534b37832008a98615db8b7b3a0f
Darren Chang <darren.ch12@gmail.com>
2020-11-04 23:38:50 -0500

    Add working midterm report and graphics.

--- eda.R
+++ eda.R
@@ -16,24 +16,61 @@ weekly_ff <- vroom("weekly_ff.csv")
 weekly.cor <- cor(select(Filter(is.numeric, weekly_ff), -c(season, game_id, week)))
 
 # -- correlation plot
-png(height=900, width=900, file="weekly.png", type = "cairo")
+png(height=1100, width=1100, file="weekly.png", type = "cairo")
 corrplot(weekly.cor,
-        title = "Correlation between Features",
+        # title = "Correlation between Features",
         method = "color",
         order = "FPC",
         # tl.pos = "td", 
-        tl.cex = 0.5,
+        tl.cex = 1,
         diag = F,
         type = "lower")
 
 dev.off()
 
 # -- histograms
-png(height=900, width=900, file="pass_yds_hist.png", type = "cairo")
+png(height = 1080, 
+    width = 2340, 
+    file = "yds_hist.png", 
+    type = "cairo-png", 
+    units = "px", 
+    res = 300)
+
+par(mfcol=c(2,3))
 hist(filter(weekly_ff, PassingYds_prev !=0)$PassingYds_prev,
         breaks = 30,
-        col = "blue",
+        col = "#30a2da",
         xlab = "Previous Week Passing Yards",
+        main = ""
+        )
+hist(filter(weekly_ff, PassingYds_cum !=0)$PassingYds_cum,
+        breaks = 30,
+        col = "#30a2da",
+        xlab = "Cumulative Passing Yards",
+        main = ""
+        )
+hist(filter(weekly_ff, RushingYds_prev !=0)$RushingYds_prev,
+        breaks = 30,
+        col = "#fc4f30",
+        xlab = "Previous Week Rushing Yards",
+        main = ""
+        )
+hist(filter(weekly_ff, RushingYds_cum !=0)$RushingYds_cum,
+        breaks = 30,
+        col = "#fc4f30",
+        xlab = "Cumulative Rushing Yards",
+        main = ""
+        )
+hist(filter(weekly_ff, ReceivingYds_prev !=0)$ReceivingYds_prev,
+        breaks = 30,
+        col = "#6d904f",
+        xlab = "Previous Week Receiving Yards",
+        main = ""
+        )
+hist(filter(weekly_ff, ReceivingYds_cum !=0)$ReceivingYds_cum,
+        breaks = 30,
+        col = "#6d904f",
+        xlab = "Cumulative Receiving Yards",
         main = ""
         )
 
